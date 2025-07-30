b152d33dbf98bed206f5e12ee91a1d96acd60186
abhiagarwal13 <161476295+abhiagarwal13@users.noreply.github.com>
2025-07-30 16:34:24 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
-function ValidateEmail(inputText)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
+console.log("This is my script")
+
+let result = {
+    "tag": "",
+    "free": false,
+    "role": false,
+    "user": "abhiagarwal",
+    "email": "abhiagarwal@gmail.com",
+    "score": 0.64,
+    "state": "undeliverable",
+    "domain": "gmail.com",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
 }
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
-}
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_oXrKw5XfvoEJw03I3kzksNfrDhnCi3NjLKxsBvXQ"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
